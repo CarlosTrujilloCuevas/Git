e6857f838083737beb8c996463e65a46d4805cc1
CarlosTrujilloCuevas <ctcode.90@gmail.com>
2018-09-09 21:51:49 -0300

    functionSuma
    Se creo la funcion suma

---
+++ README.md
@@ -0,0 +1,5 @@
+#Cambiando y manejando versiones
+
+**Intento N°2**
+_Cambios_
+Aun no entiendo el orden  de esto
---
+++ main.js
@@ -0,0 +1,8 @@
+let a, b; 
+function suma(a,b) {
+    let suma = a + b;
+    return suma;
+}
+
+let resultado = suma(a,b);
+console.log(resultado);
